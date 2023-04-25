8146b5ee80f48dfdce2e669a996f78717ffabfed
Bruno Policarpo <9388621+bptfreitas@users.noreply.github.com>
2023-04-25 16:49:23 -0300

    Added the class VirtualBotParameters to read the compile-time definitions from virtualbot.h

--- virtualbot_serial/tests/tests_virtualbot_driver.py
+++ virtualbot_serial/tests/tests_virtualbot_driver.py
@@ -12,23 +12,44 @@ class VirtualBotParameters:
 
         self.__parameters = {}
 
-        self.__fileName = '../include/virtualbot.h'
+        self.__fileName = 'include/virtualbot.h'
 
         with open(self.__fileName, 'r') as f:
-            for line in f.read():
 
-                contents = line.split(" ").strip()
+            for line in f.readlines():
 
-                try:
+                contents = [ directive.strip() for directive in line.split(" ") ]
+
+                try:                    
                     index = contents.index("#define")
                 except ValueError:
                     continue
 
                 if contents[ index + 1 ].strip() == "VIRTUALBOT_MAX_SIGNAL_LEN":
-                    self.__parameters["VIRTUALBOT_MAX_SIGNAL_LEN"] = int( contents[ index + 1 ].strip() )
+                    self.__parameters["VIRTUALBOT_MAX_SIGNAL_LEN"] = int( contents[ index + 2 ].strip() )
 
                 elif contents[ index + 1 ].strip() == "VIRTUALBOT_TOTAL_SIGNALS":
-                    self.__parameters["VIRTUALBOT_TOTAL_SIGNALS"] = int( contents[ index + 1 ].strip() )
+                    self.__parameters["VIRTUALBOT_TOTAL_SIGNALS"] = int( contents[ index + 2 ].strip() )
+
+                elif contents[ index + 1 ].strip() == "VIRTUALBOT_MAX_TTY_MINORS":
+                    self.__parameters["VIRTUALBOT_MAX_TTY_MINORS"] = int( contents[ index + 2 ].strip() )
+
+                elif contents[ index + 1 ].strip() == "VIRTUALBOT_TTY_MAJOR":
+                    self.__parameters["VIRTUALBOT_TTY_MAJOR"] = int( contents[ index + 2 ].strip() )
+
+                elif contents[ index + 1 ].strip() == "VIRTUALBOT_TTY_NAME":
+                    self.__parameters["VIRTUALBOT_TTY_NAME"] = contents[ index + 2 ].strip()
+
+                elif contents[ index + 1 ].strip() == "VB_COMM_TTY_NAME":
+                    self.__parameters["VB_COMM_TTY_NAME"] = contents[ index + 2 ].strip()                    
+
+                # now for the comm part
+
+        sys.stdout.write( "VirtualBot compile-time parameters: \n" )
+        for directive in self.__parameters.keys(): 
+            sys.stdout.write( directive + "\t" + str(self[directive])  + "\n" )
+
+        sys.stdout.write( "\n" )
 
     def __getitem__(self, index):
         return self.__parameters[index]
@@ -54,6 +75,10 @@ import unittest
 
 class TestSerialObject(unittest.TestCase):
 
+    def setUp(self):
+
+        self.__VBParams = VirtualBotParameters()
+
 	# Check is you can instantiate a Serial object with the VirtualBot driver
     def test_01_SerialObjectInstantiated(self):
         self.assertIsInstance( serial.Serial("/dev/ttyVB0", 9600) , serial.Serial )
@@ -69,5 +94,6 @@ class TestSerialObject(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
  """
+
 if __name__ == '__main__':
     unittest.main()
